9c67d6afd85d6e6b9b91f24907cd1493c0fe51c2
Halid Mohamed <150917290+itsfornothing@users.noreply.github.com>
2026-05-21 09:30:09 +0300

    Delete jest-divide-app directory

--- jest-divide-app/calculator.js
+++
@@ -1,9 +0,0 @@
-const Divide = require("./divide");
-
-
-function add(a, b) { return a + b; }
-function subtract(a, b) { return a - b; }
-function multiply(a, b) { return a * b; }
-function divide(a, b) { return Divide(a, b) }
-
-module.exports = { add, subtract, multiply, divide };
